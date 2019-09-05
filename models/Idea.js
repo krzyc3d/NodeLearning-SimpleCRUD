ad2cc8a2f91cee5bdffd013101b04129b2c579eb
krzyc3d <krzych3d@wp.pl>
2019-09-05 09:56:05 +0200

    feat(): simple crud app  first commit

---
+++ models/Idea.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+//create Schema
+const IdeaSchema = new Schema({
+    title:{
+        type: String,
+        require: true
+    },
+    details: {
+        type: String,
+        require: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+//Schema registaration
+mongoose.model('ideas', IdeaSchema);
