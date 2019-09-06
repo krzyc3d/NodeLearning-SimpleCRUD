e3c034a937705c75eaae52fd38439f630d0eed3f
krzyc3d <krzych3d@wp.pl>
2019-09-06 10:22:23 +0200

    feat(): add registration form validation

--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,28 @@ router.get('/register', (req,res) => {
     res.render('users/register');
 })
 
+// Register form POST
+router.post('/register', (req, res) => {
+    let errors = [];
+
+    if( req.body.password != req.body.password2 ) {
+        errors.push({text:'Passwords do not match'});
+    }
+    if( req.body.password.length < 6 ) {
+        errors.push({text:'Passwords must be at least 6 characters'});
+    }
+    if( errors.length > 0 ) {
+        res.render('users/register', {
+            errors: errors,
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            password2: req.body.password2
+        });
+    } else {
+        res.send('passed');
+    }
+});
+
+
 module.exports = router;
