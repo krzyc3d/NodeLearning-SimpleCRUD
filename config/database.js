a77a70135e12685e15d5420afe2519504a2718a1
krzyc3d <krzych3d@wp.pl>
2019-09-09 12:33:19 +0200

    heroku deploy db changes

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'production'){
     module.exports = {mongoURI:
-    'mongodb+srv://krzych3d:<password>@node-training-vrodr.mongodb.net/test?retryWrites=true&w=majority'}
+    'mongodb+srv://krzych3d:Ytrewq321!@node-training-vrodr.mongodb.net/test?retryWrites=true&w=majority'}
 } else {
     module.exports = {mongoURI:'mongodb://localhost/node-training'}
 }
