e2bd7536025186e7bd802e8995694e07deed4368
krzyc3d <krzych3d@wp.pl>
2019-09-09 12:30:01 +0200

    heroku deploy db changes

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'production'){
     module.exports = {mongoURI:
-    'mongodb://krzych3d:Ytrewq321!@node-training-shard-00-00-vrodr.mongodb.net:27017,node-training-shard-00-01-vrodr.mongodb.net:27017,node-training-shard-00-02-vrodr.mongodb.net:27017/test?ssl=true&replicaSet=node-training-shard-0&authSource=admin&retryWrites=true&w=majority'}
+    'mongodb+srv://krzych3d:<password>@node-training-vrodr.mongodb.net/test?retryWrites=true&w=majority'}
 } else {
     module.exports = {mongoURI:'mongodb://localhost/node-training'}
 }
