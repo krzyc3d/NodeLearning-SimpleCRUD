5531c053e398c5e436fa16ee14b3dcb5971f17f2
krzyc3d <krzych3d@wp.pl>
2019-09-09 13:27:00 +0200

    fix(): try to fix db connection 3

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI:'mongodb://krzych3d:Ytrewq321!@node-training-vrodr.mongodb.net/test?retryWrites=true&w=majority'}
+    module.exports = {mongoURI: 'mongodb+srv://krzych3d:Ytrewq321!@node-training-vrodr.mongodb.net/test?retryWrites=true&w=majority'}
 
 } else {
-    module.exports = {mongoURI:'mongodb://localhost/node-training'}
+    module.exports = {mongoURI: 'mongodb://localhost/node-training'}
 }
